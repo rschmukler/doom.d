efde4535a8157a6294c3cacadce550b4e61970d6
Ryan Schmukler <rschmukler@gmail.com>
2018-02-24 20:57:49 -0500

    feat: add white space pruning

--- init.el
+++ init.el
@@ -32,6 +32,12 @@
 ;; Auto revert-mode. Look ma, no hands...
 (global-auto-revert-mode t)
 
+(setq
+ whitespace-line-column 100
+ whitespace-style
+ '(face trailing lines-tail))
+(global-whitespace-mode)
+
 ;; Turn off line wrapping
 (setq-default truncate-lines 1)
 
