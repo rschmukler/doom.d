363d677d9d972121f84251a014315704475f761c
Ryan Schmukler <rschmukler@gmail.com>
2019-07-30 18:00:29 -0400

    feat: add keychain-refresh-environment fn
    Adds a function to reload ssh-agent based environment variables.
    Basically needed to run 1x per session. TODO: Figure out how to have it
    autorun on client attach.

--- +functions.el
+++ +functions.el
@@ -186,3 +186,21 @@ current buffer directory."
 (define-move-and-insert grfn/insert-at-form-end
   (forward-sexp)
   (insert " "))
+
+(defun keychain-refresh-environment ()
+  "Set ssh-agent and gpg-agent environment variables.
+Set the environment variables `SSH_AUTH_SOCK', `SSH_AGENT_PID'
+and `GPG_AGENT' in Emacs' `process-environment' according to
+information retrieved from files created by the keychain script."
+  (interactive)
+  (let* ((ssh (shell-command-to-string "keychain -q --noask --agents ssh --eval"))
+         (gpg (shell-command-to-string "keychain -q --noask --agents gpg --eval")))
+    (list (and ssh
+               (string-match "SSH_AUTH_SOCK[=\s]\\([^\s;\n]*\\)" ssh)
+               (setenv       "SSH_AUTH_SOCK" (match-string 1 ssh)))
+          (and ssh
+               (string-match "SSH_AGENT_PID[=\s]\\([0-9]*\\)?" ssh)
+               (setenv       "SSH_AGENT_PID" (match-string 1 ssh)))
+          (and gpg
+               (string-match "GPG_AGENT_INFO[=\s]\\([^\s;\n]*\\)" gpg)
+               (setenv       "GPG_AGENT_INFO" (match-string 1 gpg))))))
