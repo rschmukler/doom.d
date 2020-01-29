34b6dd8df059573c45b96e4632cebe078360131b
Ryan Schmukler <rschmukler@gmail.com>
2020-01-29 16:32:48 -0500

    refactor: cleanup custom evil-matchit recipe
    Remove dependency on old evil-matchit. Melpa is fixed

--- packages.el
+++ packages.el
@@ -23,7 +23,6 @@
 (package! company-lsp)
 (package! dockerfile-mode)
 (package! aggressive-indent)
-(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))
 (package! lispyville)
 (package! reason-mode)
 (package! ob-ipython)
